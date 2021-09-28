4d8bbc710d379b5986dac63c19c8064a90396b73
thomas <tmeyerwebdev@gmail.com>
2021-09-28 00:08:49 +0000

    Added get route to  to render  urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
@@ -19,8 +23,6 @@ app.get("/urls/:shortURL", (req, res) =>{
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
-
-
 
 
 app.get("/", (req, res) => {
