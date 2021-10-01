3bbc0112388bec877db635abd3710339a1870d2f
thomas <tmeyerwebdev@gmail.com>
2021-10-01 03:12:03 +0000

    refactor and cleand up code

--- test/helperTest.js
+++ test/helperTest.js
@@ -4,30 +4,28 @@ const { searchUsersByEmail } = require('../helper.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('searchUsersByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = searchUsersByEmail("user@example.com", testUsers)
+    const user = searchUsersByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
-    //console.log(result);
     assert.equal(user.id, expectedOutput);
   });
 });
 
 describe('searchUsersByEmail', function() {
-  it(' If we pass in an none existant email return undefined.', function() {
-    const user = searchUsersByEmail("u@example.com", testUsers)
-    
+  it('If passed an none existant email will return undefined.', function() {
+    const user = searchUsersByEmail("u@example.com", testUsers);
     assert.isUndefined(user);
   });
 });
