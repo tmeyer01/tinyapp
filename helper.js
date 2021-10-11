d9079b63e3846194bc96465dfb539313a9b6709a
thomas <tmeyerwebdev@gmail.com>
2021-10-11 20:27:45 +0000

    Added new functions

--- helper.js
+++ helper.js
@@ -8,6 +8,26 @@ const searchUsersByEmail = (email, database) => {
   return undefined;
 };
 
+const searchDataBaseByUser = (userID, database) => {
+  const newUrlDatabase = {};
+  for (const shortURL in database) {
+    if (database[shortURL]["userID"] === userID) {
+      newUrlDatabase[shortURL] = database[shortURL];
+    }
+  }
+  return newUrlDatabase;
+};
+
+const generateRandomString = () => {
+  let sixRanLetters = "";
+  const letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSdTtUuVvWwXxYyZz";
+  const letterNum = letters.length;
+
+  for (let i = 0; i < 6; i++) {
+    sixRanLetters += letters.charAt(Math.floor(Math.random() * letterNum));
+  }
+  return sixRanLetters;
+};
 
 
-module.exports = { searchUsersByEmail };
+module.exports = { searchUsersByEmail, searchDataBaseByUser, generateRandomString };
